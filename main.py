5bf5325c211165b9fbaa4f8d089510fe7faac101
MuthuTej <muthu1410raj@gmail.com>
2025-11-14 06:00:31 +0530

    new two modal have been implemented

--- main.py
+++ main.py
@@ -16,6 +16,21 @@ app = FastAPI(title="Soil Classification API")
 model = load_model("soil_mobilenet.h5")
 class_indices = joblib.load("soil_class_indices.pkl")
 classes = {v: k for k, v in class_indices.items()}
+groundwater_model = load_model("groundwater_prediction_model.h5", compile=False)
+rain_model = load_model("rainfall_prediction_model.h5", compile=False)
+
+class RainfallInput(BaseModel):
+    temperature: float
+    humidity: float
+    pressure: float
+    soil_moisture: float
+    ultrasonic_distance: float
+
+class GroundwaterRequest(BaseModel):
+    temperature: float
+    humidity: float
+    pressure: float
+    soil_moisture: float
 
 # Request model
 class ImageRequest(BaseModel):
@@ -75,6 +90,51 @@ def predict_base64(request: ImageRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@app.post("/predict_groundwater")
+def predict_groundwater(request: GroundwaterRequest):
+    try:
+        # Arrange features EXACTLY as in training
+        input_data = np.array([[
+            request.temperature,
+            request.humidity,
+            request.pressure,
+            request.soil_moisture
+        ]])
+
+        # Scale using saved scaler
+        # scaled = scaler_g.transform(input_data)
+
+        # Predict
+        pred = groundwater_model.predict(input_data)
+        groundwater_value = float(pred[0][0])
+
+        return {
+            "groundwater_level_pred": groundwater_value,
+            "note": "groundwater_level unit = same as training dataset"
+        }
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/predict-rainfall")
+def predict_rainfall(data: RainfallInput):
+    # Convert input data to array
+    input_features = np.array([[
+        data.temperature,
+        data.humidity,
+        data.pressure,
+        data.soil_moisture,
+        data.ultrasonic_distance
+    ]])
+    
+    # Scale features
+    # input_scaled = scaler_r.transform(input_features)
+    
+    # Predict
+    pred = rain_model.predict(input_features)
+    
+    # Return prediction as float
+    return {"predicted_rainfall": float(pred[0][0])}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5001)
